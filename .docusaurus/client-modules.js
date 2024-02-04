2c7d1a6f205a3d142c1bdf1c55b20c8ba2f9ce16
Domas Markevičius <domas.marko@gmail.com>
2024-02-04 17:35:18 +0200

    Added docusaurus template

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/domasmark/Documents/Repos/template-docusaurus-docs/template-docusaurus-docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/domasmark/Documents/Repos/template-docusaurus-docs/template-docusaurus-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/domasmark/Documents/Repos/template-docusaurus-docs/template-docusaurus-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/domasmark/Documents/Repos/template-docusaurus-docs/template-docusaurus-docs/src/css/custom.css"),
+];
