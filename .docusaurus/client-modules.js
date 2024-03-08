1f1179ac0a8bc3341d93845c7191c9a8bfb8c8ea
argshook <argshook@gmail.com>
2024-03-08 13:37:06 +0200

    remove gitignored files

--- .docusaurus/client-modules.js
+++
@@ -1,6 +0,0 @@
-export default [
-  require("/Users/domasmark/Documents/Repos/template-docusaurus-docs/template-docusaurus-docs/node_modules/infima/dist/css/default/default.css"),
-  require("/Users/domasmark/Documents/Repos/template-docusaurus-docs/template-docusaurus-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/Users/domasmark/Documents/Repos/template-docusaurus-docs/template-docusaurus-docs/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/Users/domasmark/Documents/Repos/template-docusaurus-docs/template-docusaurus-docs/src/css/custom.css"),
-];
